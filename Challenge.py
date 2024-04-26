81b54fe4ff8b7c1af06e53a4ea7669b8481bad8f
Guilherme Henrique Florêncio da Silva <guilherme.florencio@certsys.com.br>
2024-04-25 21:40:48 -0300

    Changing robot structure

--- Challenge.py
+++ Challenge.py
@@ -1,80 +1,96 @@
-from robocorp.tasks import task
 from RPA.Browser.Selenium import Selenium
+from robocorp import workitems
+import os
 import logging
-from time import sleep
-import requests
-import pandas as pd
 from datetime import datetime
+import pandas as pd
+import requests
+from time import sleep
+from robocorp.tasks import task
+
+from robocorp.tasks import task
+from robocorp import storage
+from pathlib import Path
+import re
+
 from robocorp import workitems
 from robocorp.tasks import task
-import os
+from RPA.Excel.Files import Files as Excel
 
-browser = Selenium()
+from robocorp.workitems import Inputs, Outputs
 
-@task
-def create_work_item():
-    # =-=-==-=- Creating an output work item with a payload -=-=-=-=-=
-    workitems.outputs.create(payload={"search_phrase": "Breaking News"})
+class NewsRobot:
+    def __init__(self):
+        self.browser = Selenium()
+        self.username = os.getlogin()
+        self.time_execution = datetime.now().strftime("%Y%m%d%H%M%S")
+        self.setup_logging()
 
-@task
-def open_site(url):
-    try:
-        # =-=-=-= Open Browser =-=-=-= 
-        logging.info(f'Opening the Website: {url}')
-        browser.open_available_browser(url)
-        
-        # =-=-=-= Waiting for browser to load =-=-=-= 
-        browser.wait_until_element_is_visible('//*[@id="Page-header-trending-zephr"]/div[1]/div[3]/bsp-search-overlay/button', timeout=45)
-    except Exception as e:
-        print(f"It was not possible to load the website within the timeout period: {e}")
-        raise
-@task
-def searching_news():
-    # =-=-==-=- Loop through all available input work items -=-=-=-=-=
-    for item in workitems.inputs:
-        # =-=-==-=- Access the payload of the work item -=-=-=-=-=
-        search_phrase = item.payload.get("search_phrase")
-        print(f"Search phrase is: {search_phrase}")
-        # After processing, mark the item as done
-        item.done()
-    try:
-        # =-=-=-= Click on "Magnifier" to set the text =-=-=-= 
-        browser.click_element('//*[@id="Page-header-trending-zephr"]/div[1]/div[3]/bsp-search-overlay/button')
-        logging.info('Clicking on "Magnifier" to set the text')
+    def setup_logging(self):
+        log_path = f'C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Output\\Logs\\robot.log_{self.time_execution}'
+        logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-        # =-=-=-= Searching for the news =-=-=-= 
-        browser.input_text(f'//*[@id="Page-header-trending-zephr"]/div[1]/div[3]/bsp-search-overlay/div/form/label/input', {search_phrase}, clear=True)
-        logging.info('Searching for the news: "Breaking News"')
-
-        # =-=-=-= Click on "Magnifier" icon to search =-=-=-= 
-        logging.info('Clicking on "Magnifier" icon to search')
-        browser.click_element('//*[@id="Page-header-trending-zephr"]/div[1]/div[3]/bsp-search-overlay/div/form/label/button')
-
-        # =-=-=-= Waiting for page to load =-=-=-= 
-        #Colocar if pagina nao carregar, parar o robô
-        browser.wait_until_element_is_visible("//div[@class='SearchResultsModule-filters-title']", timeout=40)
-        
-
-        # =-=-=-= Selecting "Newest" =-=-=-= 
-        logging.info('Selecting "Newest"')
-        browser.select_from_list_by_label("//select[@name='s']", 'Newest')
+    def open_site(self, url):
+        try:
+            logging.info(f'Opening the Website: {url}')
+            self.browser.open_available_browser(url)
+            self.browser.wait_until_element_is_visible('//*[@id="Page-header-trending-zephr"]/div[1]/div[3]/bsp-search-overlay/button', timeout=55)
+        except Exception as e:
+            logging.error(f"It was not possible to load the website within the timeout period: {e}")
+            raise
+    def create_work_items_from_excel(self):
+        excel = Excel()
+        excel.open_workbook(f"C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Input\\Config.xlsx")
+        rows = excel.read_worksheet_as_table(header=True)
     
-        sleep(4)
+        for row in rows:
+            # Assuming each row is a dictionary with 'key' and 'value' columns
+            key = row["Key"]
+            value = row["Value"]
+            workitems.outputs.create(payload={key: value})
 
-        # =-=-=-= Clicking on "Category" =-=-=-= 
-        logging.info('Clicking on "Category"')
-        browser.click_element("//div[@class='SearchFilter-heading']")
-        sleep(1)
+    def searching_news(self):
 
-        # =-=-=-= Selecting the "Stories" category =-=-=-= 
-        browser.select_checkbox("//input[@value='00000188-f942-d221-a78c-f9570e360000']")
+        for item in workitems.inputs:
+            # =-=-==-=- Access the payload of the work item -=-=-=-=-=
+            search_phrase = item.payload.get("search_phrase")
+            # After processing, mark the item as done
+            item.done()
+        
+        try:
+            # =-=-=-= Click on "Magnifier" to set the text =-=-=-= 
+            self.browser.click_element("//button[@class='SearchOverlay-search-button']")
+            logging.info('Clicking on "Magnifier" to set the text')
+            
+            # =-=-=-= Searching for the news =-=-=-=
+            self.browser.input_text(f"//input[@class='SearchOverlay-search-input']", {search_phrase}, clear=True)
+            logging.info(f'Searching for the news: "{search_phrase}"')
 
-        sleep(4)
-    except Exception as ErrorSearch:
-        print(f'Erro: {ErrorSearch}')
-        raise
-@task
-def get_news():    
+            # =-=-=-= Click on "Magnifier" icon to search =-=-=-= 
+            self.browser.click_element("//button[@class='SearchOverlay-search-submit']")
+            self.browser.wait_until_element_is_visible("//div[@class='SearchResultsModule-filters-title']", timeout=40)
+            logging.info('Search completed successfully.')
+
+            # =-=-=-= Selecting "Newest" =-=-=-= 
+            logging.info('Selecting "Newest"')
+            self.browser.select_from_list_by_label("//select[@name='s']", 'Newest')
+    
+            sleep(4)
+
+            # =-=-=-= Clicking on "Category" =-=-=-= 
+            logging.info('Clicking on "Category"')
+            self.browser.click_element("//div[@class='SearchFilter-heading']")
+            sleep(1)
+
+            # =-=-=-= Selecting the "Stories" category =-=-=-= 
+            self.browser.select_checkbox("//input[@value='00000188-f942-d221-a78c-f9570e360000']")
+
+            sleep(4)
+        except Exception as ErrorSearch:
+            logging.error(f'Error during search: {ErrorSearch}')
+            raise
+
+    def get_news(self):
         title_list = []
         descriptions_list = []
         dates = []
@@ -84,48 +100,53 @@ def get_news():
         result = False
 
         # =-=-=-= Getting the list elements =-=-=-= 
-        web_elements = browser.get_webelements("//div[@class='SearchResultsModule-results']//div[*]//div[1]//div[*]//bsp-custom-headline[1]//div[1]")
+        web_elements = self.browser.get_webelements("//div[@class='SearchResultsModule-results']//div[@class='PagePromo-content']")
         
         # =-=-=-= Getting the quantity of items =-=-=-=
         Quantity = len(web_elements)
 
-        print(f"Quantity of news", Quantity)
         logging.info(f"Quantity of news: {Quantity}")
        
         for item in range(1, Quantity + 1):
             
-            Title = browser.find_element(f"//div[@class='SearchResultsModule-results']//div[{item}]//div[1]//div[*]//bsp-custom-headline[1]//div[1]").text
-            Description = browser.find_element(f"//body/div[@class='SearchResultsPage-content']/bsp-search-results-module[@class='SearchResultsModule']/form[@class='SearchResultsModule-form']/div[@class='SearchResultsModule-ajax']/div[@class='SearchResultsModule-ajaxContent']/bsp-search-filters[@class='SearchResultsModule-filters']/div[@class='SearchResultsModule-wrapper']/main[@class='SearchResultsModule-main']/div[@class='SearchResultsModule-results']/bsp-list-loadmore[@class='PageListStandardD']/div[@class='PageList-items']/div[{item}]/div[1]/div[*]/div[1]").text
-            Date = browser.find_element(f"//div[@class='SearchResultsModule-results']//div[{item}]//div[1]//div[*]//div[2]").text
+            Title = self.browser.find_element(f"(//div[@class='SearchResultsModule-results']//span[@class='PagePromoContentIcons-text'])[{item}]").text
+            Description = self.browser.find_element(f"(//div[@class='PageList-items']//div[@class='PagePromo-description']//span[@class='PagePromoContentIcons-text'])[{item}]").text
+            Date = self.browser.find_element(f"//div[@class='SearchResultsModule-results']//div[{item}]//div[1]//div[*]//div[2]").text
+            
+            logging.info(f"Tittle: {Title}")
+            logging.info(f"Description: {Description}")
+            logging.info(f"Date: {Date}")
+            logging.info("=-=--=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
-
+            # =-=-=-= Creating Regex =-=-=-=
+            pattern = r'(\$[\d,]+(?:\.\d+)?\b|\b\d+\s*(?:dollars|usd)\b)'  # Exemplo: $10, $10.50, $111,111.11, 11 dollars, 11 USD
+            
             title_lower = Title.lower()
             description_lower = Description.lower()
-            if "$" in title_lower or "dollars" in title_lower or "usd" in title_lower \
-                or "$" in description_lower or "dollars" in description_lower or "usd" in description_lower:
+
+            if re.search(pattern,title_lower) or re.search(pattern,description_lower):
                 result = True
             else:
-                result = False
+                result = False 
 
             has_url = False
             count_title = len(Title)
             count_description = len(Description)
             try:
                 # =-=-==-=- Trying to get image URL -=-=-=-=-=                                    
-                elements = browser.get_webelements(f"//html[1]/body[1]/div[3]/bsp-search-results-module[1]/form[1]/div[2]/div[1]/bsp-search-filters[1]/div[1]/main[1]/div[3]/bsp-list-loadmore[1]/div[2]/div[{item}]/div[1]/div[1]/a[1]/picture[1]/img[1]")
+                elements = self.browser.get_webelements(f"(//div[@class='PageList-items-item']//div[@class='PagePromo-media']//img)[{item}]")
                 if elements:
-                    filename = f"C:\\Users\\{username}\\Documents\\Robots\\RPAChallenge\\Output\\downloaded_image{item}.jpg"
+                    filename = f"C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Output\\downloaded_image{item}.jpg"
                     for element in elements:
-                        url = browser.get_element_attribute(element, "src")
-                        print(f"The URL is: {url}")
+                        url = self.browser.get_element_attribute(element, "src")
                         has_url = True
                     response = requests.get(url)
                     with open(filename, 'wb') as image_file:
                         image_file.write(response.content)
                 else:
-                    print(f"The news does not have an image")
+                    logging.info(f"The news does not have an image")
             except Exception as error_image:
-                print(f"There was an error: {error_image}")
+                logging.error(f"There was an error: {error_image}")
 
             title_list.append(Title)
             descriptions_list.append(Description)
@@ -140,53 +161,56 @@ def get_news():
         # =-=-==-=- Writing date to an Excel File -=-=-=-=-=     
         date_for_excel = {"Title": title_list, "Description": descriptions_list, "Update News": dates, "UrlPath": stored_url_list,"Title Count Phrases": count_titles_list, "Description Count Phrases": count_descriptions_list, "Amount of money": result}  
         df = pd.DataFrame(date_for_excel)
-        df.to_excel(f"C:\\Users\\{username}\\Documents\\Robots\\RPAChallenge\\Output\\News_{time_execution}.xlsx", index=False)
+        df.to_excel(f"C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Output\\News_{self.time_execution}.xlsx", index=False)
+
+    def check_and_create_folder(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def upload_artifacts(self):
+        # Assuming 'output' is the directory where your files are saved
+        output_dir = Path(f'C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Output\\')
+        outputs = Outputs()
+
+        for file_path in output_dir.iterdir():
+            if file_path.is_file() and file_path.suffix in ['.jpg', '.xlsx']:
+                # =-=-==-=- Construct a unique name for the artifact in the cloud -=-=-=-=-=
+                artifact_name = f"artifact-{file_path.name}"
+                # =-=-==-=- Create a new output work item (or use an existing one) -=-=-=-=-=
+                output_item = outputs.create(save=False)
+                # =-=-==-=- Attach the file to the output work item -=-=-=-=-=
+                output_item.add_file(file_path, name=artifact_name)
+                # Save the work item to upload the file and finalize changes
+                output_item.save()
+                
+
+    def main_task(self):
+
+        path_folder = f"C:\\Users\\{self.username}\\Documents\\Robots\\RPAChallenge\\Output\\Logs"
+        self.check_and_create_folder(path_folder)
+
+        Attempts = 0
+        MaxAttempts = 3
+        while Attempts < MaxAttempts:
+            try:
+                self.open_site("https://apnews.com/")
+                self.searching_news()
+                self.get_news()
+                self.upload_artifacts()
+                logging.info('Robot executed successfully.')
+                break
+            except Exception as ErrorAttempt:
+                Attempts +=1
+                logging.error(f'Attempt {Attempts} failed with error: {ErrorAttempt}')
+            finally:
+                self.browser.close_all_browsers()
+
+        if Attempts == MaxAttempts:
+            logging.error('Maximum attempts reached, robot execution failed.')
+
 
 @task
-def check_and_create_folder(path):
-    # =-=-==-=- Checks if the folder path exists -=-=-=-=-=
-    if not os.path.exists(path):
-        # =-=-==-=- If it doesn't exist, create the folder -=-=-=-=-=
-        os.makedirs(path)
-
-@task
-def log():
-    logging.basicConfig(filename=f'C:\\Users\\{username}\\Documents\\Robots\\RPAChallenge\\Output\\Logs\\robot.log_({time_execution})', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# =-=-==-=- Getting current date -=-=-=-=-=
-now = datetime.now() # current date and time
-# =-=-==-=- Formatting current date -=-=-=-=-=
-time_execution = datetime.now().strftime("%Y%m%d%H%M%S") # Obtaining the unique identifier of each execution "time_execution"
-print(f"Time execution: {time_execution}")
-
-# =-=-==-=- Get the machine user -=-=-=-=-=
-username = os.getlogin()
-
-#  =-=-==-=- Calling the functions -=-=-=-=-=
-path_folder = f"C:\\Users\\{username}\\Documents\\Robots\\RPAChallenge\\Output\\Logs"
-check_and_create_folder(path_folder)
-create_work_item()
-log()
-
-@task
-def main_task():
-    Attempts = 0
-    MaxAttempts = 3
-    while Attempts < MaxAttempts:
-        try:
-            open_site("https://apnews.com/")
-            searching_news()
-            get_news()
-            print("Robot executed successfully.")
-            logging.info('Robot executed successfully.')
-            break
-        except Exception as ErrorAttempt:
-            Attempts +=1
-            logging.error(f'{ErrorAttempt}')
-            logging.error(f"Attempt {Attempts} failed with error: {ErrorAttempt}")
-        finally:
-            browser.close_all_browsers()
-
-    if Attempts == MaxAttempts:
-        print("Maximum attempts reached, robot execution failed.")
+def run_news_robot():
+    robot = NewsRobot()
+    robot.main_task()
 
